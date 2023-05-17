30f58b03c1e7a62f2ac4c8b75f45172af4ae43c3
Kartik Agarwal <kartikhustles@gmail.com>
2023-05-17 21:59:59 +0530

    Create ConnectedGraph.java

---
+++ Graph/ConnectedGraph.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class ConnectedGraph {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                bfsUtil(graph, vis);
+            }
+        }
+    }
+
+    public static void bfsUtil(ArrayList<Edge>[] graph, boolean visit[]) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length];
+        q.add(0); // source = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) { // visit curr
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph) {
+        boolean visit[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            dfsUtil(graph, i, visit);
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean vis[]) {
+        // visit
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                dfsUtil(graph, e.dest, vis);
+            }
+        }
+    }
+}
