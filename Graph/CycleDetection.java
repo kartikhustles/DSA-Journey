8af28a29f1ab8bd6e30652ebdd033e85175c075b
Kartik Agarwal <kartikhustles@gmail.com>
2023-05-18 21:57:34 +0530

    Updated

---
+++ Graph/CycleDetection.java
@@ -0,0 +1,11 @@
+public class CycleDetection {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+}
