6bd4ddac2de853aafd07cb9ce48f17bc7542a786
Kartik Agarwal <kartikhustles@gmail.com>
2023-05-20 00:25:57 +0530

    Create Basics.java

---
+++ Backtracking/Basics.java
@@ -0,0 +1,26 @@
+public class Basics {
+    public static void changeArr(int[] arr, int i, int val) {
+        // base case
+        if (i == arr.length) {
+            printArr(arr);
+            return;
+        }
+        // recursive function
+        arr[i] = val;
+        changeArr(arr, i + 1, val + 1);
+        arr[i] = arr[i] - 2; // backtracking step
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new arr[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
